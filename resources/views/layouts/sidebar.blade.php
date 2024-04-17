c9986c151efd3afe023cb4a161f06b2751524a16
librankaio <95515543+librankaio@users.noreply.github.com>
2024-04-17 16:01:55 +0700

    update sidebar 17042024

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,13 @@
 <ul class="sidebar-menu">
     <li class="menu-header">Menu</li>
     <li class="nav-item dropdown">
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-cubes"></i><span>Main Menu</span></a>
+        {{-- <a href="#" class="nav-link has-dropdown"><i class="fas fa-cubes"></i><span>Main Menu</span></a>
         <ul class="dropdown-menu">
             <li><a class="nav-link" href="{{ route('customer') }}">Data Customer</a></li>       
             <li><a class="nav-link" href="{{ route('produk') }}">Produk</a></li>  
             <li><a class="nav-link" href="{{ route('penjualan') }}">Penjualan</a></li>  
-        </ul>
+        </ul> --}}
+        <li><a class="nav-link" href="{{ route('customer') }}"><i class="fas fa-cubes"></i> <span>Data Customer</span></a></li>
+        <li><a class="nav-link" href="{{ route('produk') }}"><i class="fas fa-cubes"></i> <span>Produk</span></a></li>
+        <li><a class="nav-link" href="{{ route('penjualan') }}"><i class="fas fa-cubes"></i> <span>Penjualan</span></a></li>
     </li>
