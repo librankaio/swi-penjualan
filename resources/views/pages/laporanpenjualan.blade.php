1516890398d97c76603cc461842663f1b6eee9e7
librankaio <95515543+librankaio@users.noreply.github.com>
2024-04-23 14:23:25 +0700

    update report penjualan 23042024

--- resources/views/pages/laporanpenjualan.blade.php
+++ resources/views/pages/laporanpenjualan.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 <section class="section">
     <div class="section-header">
-        <h1>Laporan Stock</h1>
+        <h1>Laporan Penjualan</h1>
         <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="#">Report</a></div>
             <div class="breadcrumb-item"><a class="text-muted">Laporan Penjualan</a></div>
